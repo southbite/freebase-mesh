be645db3157f07c835d56dc13a6cfe8d93a3a1f1
nomilous <richard@clue.co.za>
2015-05-08 15:22:42 +0200

    so far

--- test/e3e_mesh.js
+++ test/e3e_mesh.js
@@ -2,28 +2,46 @@ ipso = require('ipso');
 
 describe('Mesh', function() {
 
-    before(ipso(function(done, Mesh) {
+  before(ipso(function(done, Mesh) {
 
-        this.config = {
+    this.config = {
 
-            
+      dataLayer: {
+        authTokenSecret: 'a256a2fd43bf441483c5177fc85fd9d3',
+        systemSecret: 'mesh',
+        log_level: 'info|error|warning'
+      },
 
-        };
+      remoteEndpoints: {},
 
-        this.mesh = Mesh();
-        this.mesh.initialize( this.config, function(err) { 
+      modules: {},
 
-            done();
+      components: {
 
-        });
+        emitter: {}
 
-    }));
+      },
 
 
-    it('starts', ipso(function() {
 
-        console.log(this.mesh);
+    };
+
+    this.mesh = Mesh();
+    this.mesh.initialize( this.config, function(err) {
+
+      if (err) console.log(err.stack);
+
+      done();
+
+    });
+
+  }));
+
+
+  it('starts', ipso(function() {
+
+    //console.log(this.mesh);
         
-    }));
+  }));
 
 });
