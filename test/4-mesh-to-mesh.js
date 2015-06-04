ece64c88ffe2ff321d6a1650c8062ec9d9aaae14
nomilous <richard@clue.co.za>
2015-06-04 17:35:45 +0200

    mesh to mesh test

--- test/4-mesh-to-mesh.js
+++ test/4-mesh-to-mesh.js
@@ -1,91 +1,25 @@
 // cannot do mocha test/4-mesh-to-mesh.js --watch
 // address already in use for 2nd... runs
 
-var maximumPings = 1000;
+var Mesh = require('../lib/system/mesh')
+  , spawn = require('child_process').spawn
+  , sep = require('path').sep
+  , remote
+  , assert = require('assert')
+  ;
 
-var Mesh = require('../lib/system/mesh');
+mesh = Mesh();
 
-mesh1 = Mesh();
-mesh2 = Mesh();
-
-config1 = {
-  name: 'mesh1',
-  port: 3001,
-  dataLayer: {
-    authTokenSecret: 'a256a2fd43bf441483c5177fc85fd9d3',
-    systemSecret: 'mesh',
-    log_level: 'info|error|warning'
-  },
-  endpoints: {
-    /* TODO Race condition on miltiple mesh startup and connect to each other
-     *      need a retry loop in the endpoint connect
-     */
-  },
-  modules: {
-    "module1":{
-      path:__dirname + "/4-module1",
-      constructor:{
-        type:"sync",
-        parameters:[]
-      }
-    },
-    "freebaseUI":{
-      path:"freebase-ui-module",
-      constructor:{
-        type:"sync",
-        context:"new"//instantiate as a new one
-        // parameters:[]
-      }
-    }
-  },
-  components: {
-    "component1":{
-      moduleName:"module1",
-      // scope:"component",
-      // startMethod:"start",
-      schema:{
-        "exclusive":false,//means we dont dynamically share anything else
-        "methods":{
-          "thanksYou": {
-            parameters: [
-              {name:'arg1',required:true},
-              {name:'arg1',required:true},
-              {name:'callback', type:'callback', required:true}
-            ]
-          }
-        }
-      }
-    },
-    "freebaseUI":{
-      moduleName:"freebaseUI",
-      // startMethod:"start",
-      scope:"module",
-      config:{},
-      schema:{
-        "exclusive":false,//means we dont dynamically share anything else
-        "methods":{
-          // start: {
-          //   type: 'async',
-          //   parameters: [
-          //     //{"required":true, "value":{"freebase-system-secret":"mesh","freebase-port":3001}}
-          //   ]
-          // }
-        }
-      }
-    }
-  }
-};
-
-config2 = {
+config = {
   name: 'mesh2',
-  port: 3002,
   dataLayer: {
+    port: 3002,
     authTokenSecret: 'a256a2fd43bf441483c5177fc85fd9d3',
     systemSecret: 'mesh',
     log_level: 'info|error|warning'
   },
   endpoints: {
-    mesh1: {
+    theFarawayTree: {  // remote mesh node
       config: {
         port: 3001,
         secret: 'mesh',
@@ -98,40 +32,60 @@ config2 = {
 };
 
 describe('Mesh to Mesh', function() {
-
+  
   before(function(done) {
 
-    mesh1.initialize(config1, function(err) {
-      if (err) return done(err);
-      // mesh1.start(function(err) {
-      //   console.log(err);
-      // });
+    // spawn remote mesh in another process
 
-      mesh1.api.exchange.freebaseUI.start({"freebase-system-secret":"mesh","freebase-port":3001}, function(err) {
+    remote = spawn('node',[__dirname + sep + '4-first-mesh']);
+    remote.stdout.on('data', function(data) {
+
+      ////////////console.log('Remote:',data.toString());
+      if (!data.toString().match(/READY/)) return;
+
+      // once it says READY start local mesh
+
+      mesh.initialize(config, function(err) {
         if (err) return done(err);
+        done();
       });
-
-      // mesh2.initialize(config2, function(err) {
-      //   if (err) return done(err);
-      //   done();
-      // });
-
     });
   });
 
+
   after(function(done) {
-    // TODO shut down mesh ability
+    remote.kill();
     done();
   })
 
-  it('',function(done) {
-    this.timeout(200000000);
+  context('the faraway tree', function() {
 
+    it("we can ride moonface's slippery slip",function(done) {
 
-    // done();
+      mesh.api.exchange
+      .theFarawayTree.moonface.rideTheSlipperySlip(
+        'one!', 'two!', 'three!', function(err, res) {
+
+          assert(res == 'one! two! three!, wheeeeeeeeeeeeheeee!');
+          done()
+
+      });
+    });
+
+    it('we know when there was an accident', function(done) {
+
+      mesh.api.exchange
+      .theFarawayTree.moonface.haveAnAccident(function(err, res) {
+
+        // haveAnAccident() throws on the other side
+
+        assert(err.toString().match(/SlipFailure: Stray patch of glue./))
+        done();
+
+      });
+
+    });
+
   });
-
-
-
 
 });
