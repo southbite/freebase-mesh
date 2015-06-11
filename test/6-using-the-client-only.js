c20ccdde21bde2b73541f1084dc4d4899390054a
nomilous <richard@clue.co.za>
2015-06-11 13:57:01 +0200

    initialize

--- test/6-using-the-client-only.js
+++ test/6-using-the-client-only.js
@@ -1,6 +1,36 @@
 describe('Using the clientside only', function() {
 
+  var spawn = require('child_process').spawn;
+  var sep = require('path').sep;
+  var remote;
+
+  // Spawn mesh in another process.
+  before(function(done) {
+    remote = spawn('node',[__dirname + sep + '4-first-mesh']);
+    remote.stdout.on('data', function(data) {
+      if (!data.toString().match(/READY/)) return;
+      done();
+    });
+  });
+  after(function(done) {
+    remote.kill();
+    done();
+  })
+
+  context('with clientside bits', function() {
+
+
+    it('', function(done) {
+      
+      var freebase = require('freebase');
+      var api = require('../../lib/ststem/api');
+
+      api.initialize(config, freebase, function(err, api) {
+
+      })
 
 
 
+    });
+  })
 });
